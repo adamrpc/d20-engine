ff2c97b5e6d6cd5b6b823a66d731794e5d806354
AdamRPC <adamrpcs@gmail.com>
2017-01-16 15:18:20 +0100

    Fix perk references in status model.

--- app/status/services/AbstractStatus.js
+++ app/status/services/AbstractStatus.js
@@ -1,11 +1,11 @@
 'use strict';
 
-angular.module( 'd20-engine' ).factory( 'AbstractPerk', function( $log, PerkLib ) {
+angular.module( 'd20-engine' ).factory( 'AbstractStatus', function( $log, PerkLib ) {
   var i = 0;
   function AbstractStat( name ){
     this.min = 0;
     this.max = null;
-    this.name = 'Perk-' + i;
+    this.name = 'Status-' + i;
     i++;
     this.description = '';
     PerkLib.register( name, this );
