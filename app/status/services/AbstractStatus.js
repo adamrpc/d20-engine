f37f25d7c52ab38490f5cde3698efd284aa43834
AdamRPC <adamrpcs@gmail.com>
2017-01-16 15:19:37 +0100

    Add minTime and maxTime in AbstractStatus.

--- app/status/services/AbstractStatus.js
+++ app/status/services/AbstractStatus.js
@@ -1,14 +1,16 @@
 'use strict';
 
-angular.module( 'd20-engine' ).factory( 'AbstractStatus', function( $log, PerkLib ) {
+angular.module( 'd20-engine' ).factory( 'AbstractStatus', function( $log, StatusLib ) {
   var i = 0;
   function AbstractStat( name ){
     this.min = 0;
     this.max = null;
+    this.minTime = 0;
+    this.maxTime = null;
     this.name = 'Status-' + i;
     i++;
     this.description = '';
-    PerkLib.register( name, this );
+    StatusLib.register( name, this );
   }
   AbstractStat.prototype.changed = function(libName, creature, changes) {
     $log.debug('Change detected on ' + libName, this, creature, changes);
